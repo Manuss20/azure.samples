7c20c5a7ee66a010f681f1ed1e56e4a08425140d
Manuel Sanchez <manuss20@gmail.com>
2021-08-25 14:31:44 +0200

    Update module

--- VM_Nginx_example/nic.tf
+++ VM_Nginx_example/nic.tf
@@ -7,7 +7,7 @@ resource "azurerm_network_interface" "nic" {
 
   ip_configuration {
     name                          = var.name-ipconfig-nic
-    subnet_id                     = "/subscriptions/4bc8c932-c834-4b5e-8081-2ff96cb1d4be/resourceGroups/spoke1-vnet-rg/providers/Microsoft.Network/virtualNetworks/spoke1-vnet/subnets/BackendSubnet"
+    subnet_id                     = azurerm_subnet.subnet.id
     private_ip_address_allocation = var.private-ip-allocation-nic
     public_ip_address_id          = azurerm_public_ip.publicip.id
   }
